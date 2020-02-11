5b45f545f1bb6c236039c3c34f5002a5ea20df99
John (Jack) Meyer <jack10042@gmail.com>
2020-02-10 19:35:14 -0500

    Refactor code and fix slight issues

--- Makefile
+++ Makefile
@@ -3,20 +3,27 @@ FILES=src/main/java/bot/*.java
 COOKIE=foobar
 
 build: $(FILES)
-	gradle jar
+	gradle build jar
 
 update:
 	git submodule foreach git pull origin master
 
-run:	build/
-	java -cp "lib/jsettlers2/build/libs/JSettlers-2.2.00.jar:build/classes/java/main" -Djsettlers.debug.traffic=Y bot.NDRobotClient -c $(COOKIE)
+install:	update build
 
-serve:
-	java -cp "lib/jsettlers2/build/libs/JSettlers-2.2.00.jar" soc.server.SOCServer -o N7=t7 -Djsettlers.startrobots=3 -Djsettlers.allow.debug=Y -Djsettlers.bots.cookie=$(COOKIE)
+run:	build
+	java -cp "lib/jsettlers2/build/libs/JSettlers-2.2.00.jar:build/classes/java/main" bot.NDRobotClient -c $(COOKIE)
 
-simulate:
-	java -cp "lib/jsettlers2/build/libs/JSettlers-2.2.00.jar" soc.server.SOCServer -Djsettlers.allow.debug=Y -Djsettlers.startrobots=4 -Djsettlers.bots.botgames.total=7 -Djsettlers.bots.botgames.shutdown=Y -Djsettlers.bots.cookie=$(COOKIE)
+serve:	build
+	java -cp "lib/jsettlers2/build/libs/JSettlers-2.2.00.jar" soc.server.SOCServer -o N7=t7 -Djsettlers.startrobots=3 -Djsettlers.allow.debug=Y -Djsettlers.bots.cookie=$(COOKIE) > /dev/null 2>&1
+
+simulate:	build
+	java -cp "lib/jsettlers2/build/libs/JSettlers-2.2.00.jar" soc.server.SOCServer -Djsettlers.allow.debug=Y -Djsettlers.startrobots=4 -Djsettlers.bots.percent3p=25 -Djsettlers.bots.botgames.parallel=1 -Djsettlers.bots.botgames.wait_sec=5 -Djsettlers.bots.botgames.total=10 -Djsettlers.bots.botgames.shutdown=Y -Djsettlers.bots.cookie=$(COOKIE) > /dev/null 2>&1
+
+client:	build
+	java -cp "lib/jsettlers2/build/libs/JSettlers-2.2.00.jar" soc.client.SOCPlayerClient localhost 8880
+
+test:
+	bash demo.sh
 
 clean:
-	@-rm -f JSettlers.jar
-	@-rm -rf build
+	gradle clean
--- src/main/java/bot/NDRobotDM.java
+++ src/main/java/bot/NDRobotDM.java
@@ -1,23 +1,15 @@
 package bot;
 
+import soc.game.SOCSettlement;
 import soc.robot.SOCRobotBrain;
 import soc.robot.SOCRobotDM;
 import soc.game.SOCPlayingPiece;
-import soc.game.SOCSettlement;
 import soc.robot.SOCPossibleSettlement;
 import soc.robot.SOCPossibleRoad;
-import soc.robot.SOCPossiblePiece;
 import soc.debug.D;
-import soc.game.SOCResourceSet;
-import soc.game.SOCPlayer;
 import soc.game.SOCRoutePiece;
-import soc.game.SOCRoad;
-import java.util.Iterator;
-import java.util.Vector;
-import java.util.Queue;
-import java.util.LinkedList;
-import javafx.util.Pair;
-import java.util.Stack;
+
+import java.util.*;
 
 public class NDRobotDM extends SOCRobotDM {
 
@@ -25,142 +17,194 @@ public class NDRobotDM extends SOCRobotDM {
         super(br);
     }
 
+    /**
+     * A strategy that chooses to build roads to the nearest open settlement spot and then settlements at that location
+     * @param strategy  an integer that determines which strategy is used
+     */
     @Override
     public void planStuff(int strategy) {
         D.ebug_enable();
-        D.ebugPrintln("!!! Starting our overriden planStuff method !!!");
+        D.ebugPrintln("!!! Planning our move !!!");
 
         // reset any building plan already in place
-        this.buildingPlan.clear(); 
+        buildingPlan.clear();
 
         // Grab all of bot's current board pieces
-        Vector vec = this.ourPlayerData.getPieces();
-        Iterator<SOCPlayingPiece> i = vec.iterator();
-        Vector<Integer> best_path = new Vector<Integer>();
+        Vector<SOCPlayingPiece> playerPieces = ourPlayerData.getPieces();
+        Vector<Integer> bestPath = new Vector<>();
+        Iterator<SOCPlayingPiece> i = playerPieces.iterator();
         while (i.hasNext()) {
+            //TODO is there a reason this need to be a while loop or is a for-each more idiomatic?
             SOCPlayingPiece piece = i.next();
             // look for ways to build off of any placed roads using BFS and resource odds
             if (piece.getType() == SOCPlayingPiece.ROAD) {
-                Vector<Integer> result = BFS(piece.getCoordinates());
-                D.ebugPrintln("Best on road " + piece + "was" + result.toString());
-                // has an initial "real" road inside of it so get rid of it
-                if (result.size() > 0) result.removeElementAt(0);
+                Vector<Integer> searchResult = BFS(piece.getCoordinates());
+                //TODO refactor to pass in best path in some form so that even less is explored, and to take in nodes instead of edges
+                D.ebugPrintln("Best on road " + piece + " was " + searchResult);
+                // has pre-existing road first so we remove it
+                if (searchResult.size() > 0) searchResult.removeElementAt(0);
                 // Check if better than the best path
-                if (result.size() < best_path.size() || best_path.size() == 0) {
-                    best_path = result;
-                    D.ebugPrintln("Best path set:" + best_path.toString());
-                } else if (result.size() == best_path.size() && difference_from_seven_at_node(result.lastElement()) <  difference_from_seven_at_node(best_path.lastElement())){
-                    best_path = result;
+                if (comparePaths(searchResult, bestPath) > 1) {
+                    bestPath = searchResult;
+                    D.ebugPrintln("Best path set:" + bestPath.toString());
                 }
-            } 
+            }
         }
 
-        D.ebugPrintln("Best at end" + best_path.toString());
+        D.ebugPrintln("Best at end" + bestPath.toString());
         // change last coord to settlement as it will always be one from our BFS
-        this.buildingPlan.push(new SOCPossibleSettlement(this.ourPlayerData, best_path.lastElement(), null));
-        for (int j = best_path.size() - 2; j >= 0; j--)
-            this.buildingPlan.push(new SOCPossibleRoad(this.ourPlayerData, best_path.get(j), null));
+        buildingPlan.push(new SOCPossibleSettlement(ourPlayerData, bestPath.lastElement(), null));
+        for (int j = bestPath.size() - 2; j >= 0; j--)
+            buildingPlan.push(new SOCPossibleRoad(ourPlayerData, bestPath.get(j), null));
     }
 
-    // Returns if legally possible to hypothetically place a settlement at coord
-    public boolean can_build_settlement(final int coord) {
-        if (this.game.getBoard().settlementAtNode(coord) != null) return false;
+    /**
+     * Returns if legally possible to hypothetically place a settlement at coord
+     *
+     * @param coord the node to check
+     * @return if there is no settlement there or at any of the adjacent nodes
+     */
+    public boolean canBuildSettlement(final int coord) {
+        if (game.getBoard().settlementAtNode(coord) != null) return false;
 
         for (int i = 0; i <= 2; i++) {
             D.ebugPrintln("Trying to find adjacent nodes from node: " + coord + " in direction: " + i);
-            if (this.game.getBoard().settlementAtNode(this.game.getBoard().getAdjacentNodeToNode(coord, i)) != null)
+            if (game.getBoard().settlementAtNode(game.getBoard().getAdjacentNodeToNode(coord, i)) != null)
                 return false;
         }
         return true;
     }
 
-    // Returns if legally possible to hypothetically place a road at coord
-    public boolean can_build_road(final int edgeCoord) {
-        for (SOCRoutePiece r : this.game.getBoard().getRoadsAndShips())
-        {
-            if (edgeCoord == r.getCoordinates())
-            {
+    /**
+     * Returns if legally possible to hypothetically place a road at edgeCoord
+     *
+     * @param edgeCoord the edge to check
+     * @param sourceEdge the edge that we have gotten to this node from
+     * @return if there is no road already there
+     */
+    public boolean canBuildRoad(final int edgeCoord, final int sourceEdge) {
+        for (SOCRoutePiece r : game.getBoard().getRoadsAndShips()) {
+            if (edgeCoord == r.getCoordinates()) {
                 return false;
             }
         }
 
-        if (this.game.getBoard().settlementAtNode(edgeCoord) != null) return false;
+        // TODO replace with a 'node on edges' method call
+        List<Integer> nodesOnEdgeOne = game.getBoard().getAdjacentNodesToEdge(edgeCoord);
+        List<Integer> nodesOnEdgeTwo = game.getBoard().getAdjacentNodesToEdge(sourceEdge);
+        Optional<Integer> connectingNode = nodesOnEdgeOne.stream().filter(nodesOnEdgeTwo::contains).findFirst();
+
+        if(!connectingNode.isPresent()) return false; // edges do not touch
+
+        // Cannot build a road through a settlement that is not ours
+        SOCPlayingPiece settlement = game.getBoard().settlementAtNode(connectingNode.get());
+        if (settlement != null && !ourPlayerData.equals(settlement.getPlayer())) return false;
 
         return true;
     }
 
-    // returns abs value difference from 21 dice number at node
-    public float difference_from_seven_at_node(final int nodeCoord) {
-        int sum   = 0;
-        int count = 0;
-        for (Integer hexCoord : this.game.getBoard().getAdjacentHexesToNode(nodeCoord)) {
-            count ++; 
-            int dice_number = this.game.getBoard().getNumberOnHexFromCoord(hexCoord);
-            D.ebugPrintln("Value of Hex piece " + String.valueOf(hexCoord) + ": " + String.valueOf(dice_number));
-            // set water == 0
-            if (dice_number == -1) dice_number = 0;
-            sum += (dice_number > 7) ? dice_number - 7 : 7 - dice_number;
+    /**
+     * Returns the sum of the probabilities of the tiles surrounding a node
+     *
+     * @param nodeCoord the node to check
+     * @return the total probability
+     */
+    public int totalProbabilityAtNode(final int nodeCoord) {
+        int sum = 0;
+        for (int hexCoord : game.getBoard().getAdjacentHexesToNode(nodeCoord)) {
+            int diceNumber = game.getBoard().getNumberOnHexFromCoord(hexCoord);
+            D.ebugPrintln("Value of Hex piece " + String.valueOf(hexCoord) + ": " + String.valueOf(diceNumber));
+            // skip water
+            if (diceNumber > 12 || diceNumber <= 0) continue;
+            sum += (diceNumber > 7) ? 13 - diceNumber : diceNumber - 1;
         }
-        D.ebugPrintln("Sum after difference: " + String.valueOf(sum));
-        
-        // return average dist from 7
-        return (float)sum / (float)count;
+        D.ebugPrintln("Sum is: " + String.valueOf(sum));
+
+        return sum;
     }
 
-    // Returns "best" path to a new settlement from starting coord
-    // Returns a vector of coordinates where 0 is initial existing road
-    // and n - 1 is the optimal settlement placement found
+    /**
+     * Returns an "optimal" path to a new settlement from the starting edge coordinate
+     *
+     * @param coord the edge to branch off of
+     * @return a vector of coordinates starting with the existing road and ending with the optimal settlement
+     */
     public Vector<Integer> BFS(int coord) {
         Queue<Vector<Integer>> stack = new LinkedList<>();
-        
+
         // push initial coord
-        Vector<Integer> vec = new Vector<Integer>();
+        Vector<Integer> vec = new Vector<>();
         vec.add(coord);
         stack.add(vec);
-        
-        Vector<Integer> path = new Vector<Integer>();
+
+        Vector<Integer> bestPath = new Vector<>();
 
         while (!stack.isEmpty()) {
             Vector<Integer> current = stack.poll();
 
-            if (current.size() > path.size() && path.size() != 0) continue;
+            if (current.size() > bestPath.size() && bestPath.size() != 0) continue;
 
             // Check for possible settlements
-            for (int x : this.game.getBoard().getAdjacentNodesToEdge(current.lastElement())) {
-                D.ebugPrintln("Considering node " + x + " onto " + current);
-                if (this.can_build_settlement(x)) {
-                    D.ebugPrintln("Can build settlement at " + x);
-                    D.ebugPrintln("Average diff from seven: " + String.valueOf(difference_from_seven_at_node(x)));
-                    if (path.size() == 0 || current.size() < path.size() - 1) {
-                        path = (Vector<Integer>) new Vector<>(current);
-                        // add in the settlement to end of path
-                        D.ebugPrintln("Adding node " + String.valueOf(x) + " to " + path);
-                        path.add(x);
-                        continue;
-                    } else if (path.size() == current.size() && difference_from_seven_at_node(x) < difference_from_seven_at_node(path.lastElement())) {
-                        path = (Vector<Integer>) new Vector<>(current);
-                        // add in the settlement to end of path
-                        D.ebugPrintln("Adding node " + String.valueOf(x) + " to " + path);
-                        path.add(x);
-                        continue;
+            for (int node : game.getBoard().getAdjacentNodesToEdge(current.lastElement())) {
+                D.ebugPrintln("Considering node " + node + " onto " + current);
+                if (canBuildSettlement(node)) {
+                    D.ebugPrintln("Can build settlement at " + node);
+                    D.ebugPrintln("Sum of probabilities: " + totalProbabilityAtNode(node));
+                    if (comparePaths(current, bestPath) > 0) {
+                        bestPath = new Vector<>(current);
+                        // add the settlement to the end of the path leading here
+                        D.ebugPrintln("Adding node " + String.valueOf(node) + " to " + bestPath);
+                        bestPath.add(node);
                     }
                 }
             }
 
-            // Check for new roads to build & add to stack
-            for (int x : this.game.getBoard().getAdjacentEdgesToEdge(current.lastElement())) {
-                D.ebugPrintln("Adjacent edge to " + current.lastElement() + ": " + x);
-                if (this.can_build_road(x) && !current.contains(x)) {
-                    D.ebugPrintln("Can build road on " + x);
-                    Vector<Integer> new_vector = new Vector<>(current);
-                    new_vector.add(x);
-                    stack.add(new_vector);
+            // Check for new roads to build to add to stack
+            for (int edge : game.getBoard().getAdjacentEdgesToEdge(current.lastElement())) {
+                D.ebugPrintln("Adjacent edge to " + current.lastElement() + ": " + edge);
+                boolean isLoop = current.contains(edge);
+                if (canBuildRoad(edge, current.lastElement()) && !isLoop) {
+                    D.ebugPrintln("Can build road on " + edge);
+                    Vector<Integer> newPath = new Vector<>(current);
+                    newPath.add(edge);
+                    stack.add(newPath);
                 }
             }
 
         }
 
-        return path;
+        return bestPath;
     }
+
+    /**
+     * Compares two paths (vectors of roads followed by a settlement) for size and then quality of destination
+     *
+     * @param one the first path
+     * @param two the second path
+     * @return a positive if the first path is better, or a negative if the second path is better
+     */
+    private int comparePaths(Vector<Integer> one, Vector<Integer> two) {
+        if (two.size() == 0) {
+            return 4;
+        } else if (one.size() == 0) {
+            return -4;
+        }
+
+        if (one.size() < two.size()) {
+            return 2;
+        } else if (one.size() > two.size()) {
+            return -2;
+        }
+
+        if (totalProbabilityAtNode(one.lastElement()) > totalProbabilityAtNode(one.lastElement())) {
+            return 1;
+        } else if (totalProbabilityAtNode(one.lastElement()) < totalProbabilityAtNode(one.lastElement())) {
+            return -1;
+        }
+
+        return 0;
+        // TODO could consider relative rarities, or relative need as well
+    }
+
 
 }

2735cbc8f02fe47337904870e795b48f6ab41a8b
John (Jack) Meyer <jack10042@gmail.com>
2020-04-22 15:28:07 -0400

    Add stats script

--- Makefile
+++ Makefile
@@ -24,7 +24,7 @@ serve:
 	java -cp "$(LIBJARFILE)" $(SERVERCLASS) -o VP=f10 -Djsettlers.startrobots=3 -Djsettlers.allow.debug=Y -Djsettlers.bots.cookie=$(COOKIE) > server.log 2>&1
 
 simulate:
-	java -cp "$(LIBJARFILE)" $(SERVERCLASS) -Djsettlers.allow.debug=Y -Djsettlers.startrobots=4 -Djsettlers.bots.percent3p=100 -Djsettlers.bots.botgames.parallel=1 -Djsettlers.bots.botgames.wait_sec=5 -Djsettlers.bots.botgames.total=10 -Djsettlers.bots.botgames.shutdown=Y -Djsettlers.bots.cookie=$(COOKIE) > server.log 2>&1
+	java -cp "$(LIBJARFILE):build/classes/java/main" $(SERVERCLASS) -Djsettlers.allow.debug=Y -Djsettlers.startrobots=4 -Djsettlers.bots.percent3p=100 -Djsettlers.bots.start3p=1,$(BOTCLASS) -Djsettlers.bots.botgames.parallel=1 -Djsettlers.bots.botgames.wait_sec=1 -Djsettlers.bots.botgames.total=100 -Djsettlers.bots.botgames.shutdown=Y -Djsettlers.bots.cookie=$(COOKIE) > server.log
 
 client:
 	java -cp "$(LIBJARFILE)" $(CLIENTCLASS) localhost 8880
--- src/main/java/bot/NDRobotClient.java
+++ src/main/java/bot/NDRobotClient.java
@@ -2,6 +2,11 @@ package bot;
 
 import soc.baseclient.ServerConnectInfo;
 import soc.game.SOCGame;
+import soc.game.SOCLRPathData;
+import soc.game.SOCPlayer;
+import soc.message.SOCGameStats;
+import soc.message.SOCJoinGame;
+import soc.message.SOCLongestRoad;
 import soc.message.SOCMessage;
 import soc.robot.SOCRobotBrain;
 import soc.robot.SOCRobotClient;
@@ -56,7 +61,34 @@ public class NDRobotClient extends SOCRobotClient {
         return feats;
     }
 
-	@Override
+    @Override
+    public void treat(SOCMessage mes) {
+        if(mes.getType() == SOCMessage.GAMESTATS) {
+            super.treat(mes, true);
+        }
+        super.treat(mes);
+    }
+
+    @Override
+    protected void handleGAMESTATS(SOCGameStats mes) {
+        SOCGame game = this.games.get(mes.getGame());
+        SOCPlayer player = game.getPlayer(this.getNickname());
+        if(player.getTotalVP() == 0) {
+            return;
+        }
+        String toPrint = "STATS:" +
+            (player.getSettlements().size() + player.getCities().size()) + "," +
+            player.getSettlements().size() + "," +
+            player.getCities().size() + "," +
+            player.getLRPaths().stream().mapToInt(SOCLRPathData::getLength).max().orElse(0) + "," +
+            player.getNumKnights() + "," +
+            player.getTotalVP() + "," +
+            (player.getTotalVP() == 10 ? 1 : 0);
+        System.err.println(toPrint);
+        super.handleGAMESTATS(mes);
+    }
+
+    @Override
     public SOCRobotBrain createBrain(final SOCRobotParameters params, final SOCGame ga, final CappedQueue<SOCMessage> mq) {
         return new NDRobotBrain(this, params, ga, mq);
     }
